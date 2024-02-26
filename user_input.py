3efd10b18dd79dbb62bc36dd297320c2b59ef8bd
MHM1961 <micholaxmussa@gmail.com>
2024-02-26 04:08:35 +0300

    This is my first commit

---
+++ user_input.py
@@ -0,0 +1,6 @@
+# This program asking for the user name, age, and location and then prints out.
+name = input("Enter your full name:")
+age = input("How old are you:")
+location = input("Where do you live:")
+
+print("Hello", name + "you are ", age + "years old and live in ", location)
